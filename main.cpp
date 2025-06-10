eebb7df84b5fdc973cf9609983b581304b22a17b
lbh <1174560928@qq.com>
2025-06-10 13:54:50 +0800

    搭建框架，设置了qss。

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QFile styleFile(":/style/style.qss");
+    if (styleFile.open(QFile::ReadOnly)) {
+        QString styleSheet = QString(styleFile.readAll());
+        a.setStyleSheet(styleSheet);
+    } else {
+        qWarning() << "Failed to read style sheet!" << styleFile.errorString();
+    }
     MainWindow w;
     w.show();
     return a.exec();
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,24 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QButtonGroup>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QButtonGroup *btnGp = new QButtonGroup(this);
+    btnGp->addButton(ui->btnInfo, 0);
+    btnGp->addButton(ui->btnProduct, 1);
+    btnGp->addButton(ui->btnFinance, 2);
+    btnGp->addButton(ui->btnHonor, 3);
+    btnGp->addButton(ui->btnSettings, 4);
+
+    connect(btnGp, &QButtonGroup::idClicked, ui->stackedWidget, &QStackedWidget::setCurrentIndex);
+
+    btnGp->button(0)->setChecked(true);
+    ui->stackedWidget->setCurrentIndex(0);
+
 }
 
 MainWindow::~MainWindow()
